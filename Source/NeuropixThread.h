dce6543e574f8a547348e258e35fa7b3dd074496
Josh Siegle <joshs@alleninstitute.org>
2025-06-06 09:45:41 -0700

    Add option to cancel Neuropixels device scan

--- Source/NeuropixThread.h
+++ Source/NeuropixThread.h
@@ -78,7 +78,7 @@ public:
                  OwnedArray<Basestation>& basestations_,
                  DeviceType type_,
                  NeuropixAPIv3& api_v3_)
-        : ThreadWithProgressWindow ("Scanning for connected Neuropixels devices", true, false),
+        : ThreadWithProgressWindow ("Scanning for connected Neuropixels devices", true, true),
           neuropixThread (neuropixThread_),
           basestations (basestations_),
           type (type_),
@@ -87,6 +87,8 @@ public:
     ~Initializer() {}
 
     void run() override;
+
+    void threadComplete (bool userPressedCancel) override;
 
 private:
     NeuropixThread* neuropixThread;
