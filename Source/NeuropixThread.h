f72b2855820efae19525785cbfb05f403fac8ec0
Anjal Doshi <anjal.doshi@alleninstitute.org>
2025-06-26 16:33:06 -0700

    Bump plugin version to 0.7.3

--- Source/NeuropixThread.h
+++ Source/NeuropixThread.h
@@ -30,7 +30,7 @@
 
 #include "NeuropixComponents.h"
 
-#define PLUGIN_VERSION "0.7.2"
+#define PLUGIN_VERSION "0.7.3"
 
 #define MINSTREAMBUFFERSIZE (1024 * 32)
 #define MAXSTREAMBUFFERSIZE (1024 * 1024 * 32)
